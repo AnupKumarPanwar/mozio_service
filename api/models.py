2c5686aed97be6048423063a7c2f59ac9d3db419
Anup Kumar Panwar <1anuppanwar@gmail.com>
2022-06-19 00:11:01 +0530

    add Provider model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    lang = models.CharField(max_length=5)
+    currency = models.CharField(max_length=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
