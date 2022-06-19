7dc6e323b97773d3191fc9f76dffc995e5fe2b91
Anup Kumar Panwar <1anuppanwar@gmail.com>
2022-06-19 10:34:44 +0530

    add test for updating provider

--- api/tests/views/test_provider.py
+++ api/tests/views/test_provider.py
@@ -90,3 +90,45 @@ class TestGetProiver(TestCase):
         response = client.get(
             reverse('get_delete_update_provider', kwargs={'pk': 23}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class TestUpdateProvider(TestCase):
+    """ Test for updating an existing provider """
+
+    def setUp(self):
+        self.provider_1 = Provider.objects.create(
+            name='Provider 1', email='p1@gmail.com', phone='+91123456789', lang='EN', currency='INR')
+        self.provider_2 = Provider.objects.create(
+            name='Provider 2', email='p2@gmail.com', phone='+91123456799', lang='EN', currency='USD')
+
+        self.valid_payload = {
+            'name': 'Provider 1',
+            'email': 'p1@gmail.com',
+            'phone': '+911234567890',
+            'lang': 'EN',
+            'currency': 'INR'
+        }
+        self.invalid_payload = {
+            'name': 'Provider 2',
+            'email': '',
+            'phone': '+911234567891',
+            'lang': 'EN',
+            'currency': 'USD'
+        }
+
+    def test_valid_update_provider(self):
+        response = client.put(
+            reverse('get_delete_update_provider',
+                    kwargs={'pk': self.provider_1.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_update_provider(self):
+        response = client.put(
+            reverse('get_delete_update_provider',
+                    kwargs={'pk': self.provider_1.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
