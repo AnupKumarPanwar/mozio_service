7f0b8c1445d4313837c7e15b241f611b26b93268
Anup Kumar Panwar <1anuppanwar@gmail.com>
2022-06-19 13:02:05 +0530

    add test to update service area

--- api/tests/views/test_service_area.py
+++ api/tests/views/test_service_area.py
@@ -100,7 +100,7 @@ class TestGetServiceArea(TestCase):
 
     def test_get_sevice_area(self):
         """
-        Test for GET all providers
+        Valid test case
         """
         response = client.get(reverse('get_delete_update_service_areas', kwargs={
                               'provider_id': self.provider_1.pk, 'pk': self.service_area.pk}))
@@ -110,8 +110,60 @@ class TestGetServiceArea(TestCase):
 
     def test_unauthorized_get_sevice_area(self):
         """
-        Test for GET all providers
+        Should return unauthorised because the service area does not belongs to the provider
         """
         response = client.get(reverse('get_delete_update_service_areas', kwargs={
                               'provider_id': self.provider_2.pk, 'pk': self.service_area.pk}))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestUpdateServiceArea(TestCase):
+    """
+    Test to update service area
+    """
+
+    def setUp(self):
+        self.provider = Provider.objects.create(
+            name='Provider 1', email='p1@gmail.com', phone='+91123456789', lang='EN', currency='INR')
+
+        self.service_area = ServiceArea.objects.create(name="Area 1", price=1.2, polygon=Polygon(
+            ((0.0, 0.0), (0.0, 50.0), (50.0, 50.0), (50.0, 0.0), (0.0, 0.0))), provider=self.provider)
+
+        self.valid_payload = {
+            'name': 'Area 1',
+            'price': 3.5,
+            'polygon': [[1, 0], [0, 50], [50, 50], [50, 0], [1, 0]],
+            'provider_id': self.provider.pk,
+        }
+
+        # first and last points in the polyon must be same
+        self.invalid_payload = {
+            'name': 'Area 1',
+            'price': 3.5,
+            'polygon': [[0, 0], [0, 50], [50, 50], [50, 0], [1, 0]],
+            'provider_id': self.provider.pk,
+        }
+
+    def test_valid_update_sevice_area(self):
+        """
+        Valid test case
+        """
+        response = client.put(
+            reverse('get_delete_update_service_areas', kwargs={
+                    'provider_id': self.provider.pk, 'pk': self.service_area.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_update_sevice_area(self):
+        """
+        Fails because first and last element of the polygon are not same
+        """
+        response = client.put(
+            reverse('get_delete_update_service_areas', kwargs={
+                    'provider_id': self.provider.pk, 'pk': self.service_area.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
