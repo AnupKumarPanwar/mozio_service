093983b0b047f9701eff0b248342c915c44349bd
Anup Kumar Panwar <1anuppanwar@gmail.com>
2022-06-19 10:18:42 +0530

    add view for get all providers

--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Provider
+from .serializers import ProviderSerializer
+from rest_framework import status
+
 
 @api_view(['GET', 'POST'])
 def get_post_providers(request):
-    return Response({})
+    if request.method == 'GET':
+        providers = Provider.objects.all()
+        serializer = ProviderSerializer(providers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def get_delete_update_provider(request, pk):
     return Response({})
-
-
